1a2b5ab9ce48dfbe770ecb06fa0703564eea8aaa
Earnest-Smith <edsdevs01@gmail.com>
2018-02-04 22:01:32 -0500

    Add jinja2 templates to shop list project

--- shoplist/shoplist.py
+++ shoplist/shoplist.py
@@ -3,46 +3,25 @@ import os
 import jinja2
 import webapp2
 
-form_html = """
-<form>
-<h3>Add a Food</h3>
-<input type="text" name="food">
-%s
-<br><br><button>Add Food</button>
-</form>
-"""
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
-item_html = "<li>%s</li>"
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-shoplist_html= """
-<br>
-<br>
-<h1>Shopping List</h1>
-<ul>
-%s
-</ul>
-"""
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
-class MainPage(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
     def get(self):
-        output = form_html
-        output_hidden = ""
-
         items = self.request.get_all("food")
-        if items:
-            output_items = ""
-            for item in items:
-                output_hidden += hidden_html % item
-                output_items += item_html % item
+        self.render("shoplist.html", items = items)
 
-            output_shopping = shoplist_html % output_items
-            output += output_shopping
-
-        output = output % output_hidden
-
-        self.response.out.write(output)
 
 app = webapp2.WSGIApplication([ ('/', MainPage), ], debug=True)
