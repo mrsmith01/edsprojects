fca4fd9988e26e0d427fa4e95e3094246c327769
Earnest-Smith <edsdevs01@gmail.com>
2018-02-09 14:55:57 -0500

    add database entities to ascii project
    added database entities to ascii project and included new submissions

--- ascii project/ascii.py
+++ ascii project/ascii.py
@@ -19,10 +19,17 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Art(db.Model):
+    title = db.StringProperty(required = True)
+    art = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 class MainPage(Handler):
     def render_front(self, title="", art="", error=""):
-        self.render("front.html", title=title, art=art, error=error)
+        arts = db.GqlQuery("SELECT * FROM Art "
+                            "ORDER BY created DESC ")
 
+        self.render("front.html", title=title, art=art, error=error, arts=arts)
 
     def get(self):
         self.render_front()
@@ -32,7 +39,10 @@ class MainPage(Handler):
         art = self.request.get("art")
 
         if title and art:
-            self.write("Thank you for your submission!")
+            a = Art(title = title, art = art)
+            a.put()
+
+            self.redirect("/")
         else:
             error = "We need both a title and some artwork !"
             self.render_front(title, art, error)
